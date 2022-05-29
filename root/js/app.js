6bbe28a773d761685e6f9b307fc356959348b9a8
romoguill <romoguill@gmail.com>
2022-05-29 17:09:00 -0300

    Finished the logic to display the color scheme and hex values

--- root/js/app.js
+++ root/js/app.js
@@ -1,25 +1,42 @@
-const getData = async(colorSeed, colorScheme) => {
-  try {
-    const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorSeed}&mode=${colorScheme}`)
-    const data = await response.json()
-    return data
-  } catch(e) {
-    console.log(e)
-  }
-}
+const formEl = document.querySelector('#color-form');
+const colorsEl = document.querySelector('.colors');
+const colorCodesEl = document.querySelector('.color-codes');
 
-const renderData = async() => {
+const getData = async (colorSeed, colorScheme) => {
   try {
-    const data = await getData()
-  } catch(e) {
+    const response = await fetch(
+      `https://www.thecolorapi.com/scheme?hex=${colorSeed}&mode=${colorScheme}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
-const formEl = document.querySelector('#color-form')
+const renderData = (data) => {
+  Array.from(colorsEl.children).map((el, i) => {
+    return el.setAttribute(
+      'style',
+      `background-color:${data.colors[i].hex.value}`
+    );
+  });
 
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const formData = new FormData(formEl)
-  console.log(formData.get('seed-color'))
-}) 
+  Array.from(colorCodesEl.children).map((el, i) => {
+    return (el.innerHTML = data.colors[i].hex.value);
+  });
+};
+
+formEl.addEventListener('submit', async (e) => {
+  try {
+    e.preventDefault();
+    const formData = new FormData(formEl);
+    const colorSeed = formData.get('seed-color').slice(1);
+    const colorScheme = formData.get('color-scheme').toLowerCase();
+
+    const data = await getData(colorSeed, colorScheme);
+    renderData(data);
+  } catch (e) {
+    console.log(e);
+  }
+});
